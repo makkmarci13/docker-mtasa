78e1f14ff716f9ec08924b5ffb2022d989595270
Lars <ljnx86@gmail.com>
2017-02-15 22:51:50 +0100

    Add initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM debian:latest
+
+ADD entrypoint.sh /entrypoint.sh
+
+RUN useradd -d /home/mtasa -m mtasa && \
+    apt-get update && \
+    apt-get install -y wget unzip && \
+    cd /home/mtasa && \
+    wget -O mta.tar.gz https://linux.mtasa.com/dl/153/multitheftauto_linux_x64-1.5.3.tar.gz && \
+    tar xfz mta.tar.gz && \
+    mv multitheftauto_linux_* mtasa && \
+    rm mta.tar.gz && \
+    chown -R mtasa mtasa && \
+    chmod 777 /entrypoint.sh
+
+VOLUME /home/mtasa/mtasa/mods/deathmatch
+
+EXPOSE 22003 22005 22126
+
+USER mtasa
+
+CMD ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+cd /home/mtasa/
+
+if [ ! -e mtasa/mods/deathmatch/mtaserver.conf ]; then
+    echo "Downloading config and resources..."
+    wget -O baseconfig.tar.gz https://linux.mtasa.com/dl/153/baseconfig-1.5.3.tar.gz
+    tar xfz baseconfig.tar.gz
+    mv baseconfig/* mtasa/mods/deathmatch/
+    rmdir baseconfig
+    mkdir mtasa/mods/deathmatch/resources
+    cd mtasa/mods/deathmatch/resources/
+    wget -O resources.zip http://mirror.mtasa.com/mtasa/resources/mtasa-resources-latest.zip
+    unzip resources.zip
+    rm resources.zip
+fi
+
+# Start mtasa server
+exec /home/mtasa/mtasa/mta-server64 -n -t -u
