e22a3955194ec3c87cf3dd8b06a365254fd8c268
makkmarci13 <makkmarci03@gmail.com>
2018-08-02 17:46:06 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
-sudo usermod -u $UID mtasa
+if [ $(id -u) = 0 ]; then
+  if [[ -v UID ]]; then
+    usermod -u $UID mtasa
+  fi
+  if [[ -v GID ]]; then
+    groupmod -g $GID mtasa
+  fi
+  su-exec mtasa:mtasa /start.sh
+fi
