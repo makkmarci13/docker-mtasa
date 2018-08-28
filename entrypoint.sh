d6ee0021459625222c6b0440cb759942d66c5bc7
makkmarci13 <makkmarci03@gmail.com>
2018-08-28 12:19:14 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -e
 
-if [ $(id -u) = 0 ]; then
-  if [[ -v UID ]]; then
-    usermod -u $UID mtasa
-  fi
-  if [[ -v GID ]]; then
-    groupmod -g $GID mtasa
-  fi
+ cd /home/mtasa/
+ 
+ if [ ! -e mtasa/mods/deathmatch/mtaserver.conf ]; then
+    echo "Downloading config and resources..."
+    wget -O baseconfig.tar.gz https://linux.mtasa.com/dl/153/baseconfig-1.5.3.tar.gz
+    tar xfz baseconfig.tar.gz
+    mv baseconfig/* mtasa/mods/deathmatch/
+    rmdir baseconfig
+    mkdir mtasa/mods/deathmatch/resources
+    cd mtasa/mods/deathmatch/resources/
+    wget -O resources.zip http://mirror.mtasa.com/mtasa/resources/mtasa-resources-latest.zip
+    unzip resources.zip
+    rm resources.zip
 fi
+
+ # Start mtasa server
+exec /home/mtasa/mtasa/mta-server64 -n -t -u --maxplayers $SERVER_SLOT
