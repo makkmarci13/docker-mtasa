a6c5f371afae53067ff87dae2a457cf334f3fa89
makkmarci13 <makkmarci03@gmail.com>
2018-08-28 12:55:41 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
- cd /home/mtasa/
+cd /home/container
+
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
  
- if [ ! -e mtasa/mods/deathmatch/mtaserver.conf ]; then
+if [ ! -e mtasa/mods/deathmatch/mtaserver.conf ]; then
+    echo "Downloading server..."
+    wget -O mta.tar.gz https://linux.mtasa.com/dl/153/multitheftauto_linux_x64-1.5.3.tar.gz
+    tar xfz mta.tar.gz
+    mv multitheftauto_linux_* mtasa
+    rm mta.tar.gz
+    
     echo "Downloading config and resources..."
     wget -O baseconfig.tar.gz https://linux.mtasa.com/dl/153/baseconfig-1.5.3.tar.gz
     tar xfz baseconfig.tar.gz
@@ -16,5 +24,13 @@ set -e
     rm resources.zip
 fi
 
- # Start mtasa server
-exec /home/mtasa/mtasa/mta-server64 -n -t -u --maxplayers $SERVER_SLOT
+cd /home/container/mtasa/mods/deathmatch
+
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
+
+# Start mtasa server
+# exec /home/mtasa/mtasa/mta-server64 -n -t -u --maxplayers $SERVER_SLOT
