121d580c7ecb11f8b364ef01d3d8bfd902d64593
makkmarci13 <makkmarci03@gmail.com>
2018-08-28 13:32:56 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,6 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
  
 if [ ! -e mtasa/mods/deathmatch/mtaserver.conf ]; then
-    echo "Downloading server..."
-    wget -O mta.tar.gz https://linux.mtasa.com/dl/153/multitheftauto_linux_x64-1.5.3.tar.gz
-    tar xfz mta.tar.gz
-    mv multitheftauto_linux_* mtasa
-    rm mta.tar.gz
-    
     echo "Downloading config and resources..."
     wget -O baseconfig.tar.gz https://linux.mtasa.com/dl/153/baseconfig-1.5.3.tar.gz
     tar xfz baseconfig.tar.gz
@@ -23,8 +17,6 @@ if [ ! -e mtasa/mods/deathmatch/mtaserver.conf ]; then
     unzip resources.zip
     rm resources.zip
 fi
-
-cd /home/container/mtasa/mods/deathmatch
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
