9b978cbd08a29a01d2f94ae1d1c10134139ae1d1
makkmarci13 <makkmarci03@gmail.com>
2018-08-28 14:43:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,6 @@
 cd /home/container
 
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
- 
-if [ ! -e ./deathmatch/mtaserver.conf ]; then
-    echo "Downloading config and resources..."
-    wget -O baseconfig.tar.gz https://linux.mtasa.com/dl/153/baseconfig-1.5.3.tar.gz
-    tar xfz baseconfig.tar.gz
-    mv baseconfig/* ./mods/deathmatch/
-    rmdir baseconfig
-    mkdir ./mods/deathmatch/resources
-    cd ./mods/deathmatch/resources/
-    wget -O resources.zip http://mirror.mtasa.com/mtasa/resources/mtasa-resources-latest.zip
-    unzip resources.zip
-    rm resources.zip
-fi
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
